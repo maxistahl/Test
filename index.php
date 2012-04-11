051b3d14394254e5b73e6cf0c9614c1f3ff71627
maxi stahl <maxistahl@gmail.com>
2012-04-11 14:13:09 -0300

    nuevo index

---
+++ index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<title>hola!</title>
+</head>
+<body>
+	<header></header>
+	<section></section>
+	<aside></aside>
+	<footer></footer>
+</body>
+</html>
